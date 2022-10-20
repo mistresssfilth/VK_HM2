108d5cedebf168de7340e3d4ceeee3e20f424939
a.osipova <mistresssfilth@gmail.com>
2022-10-20 20:31:32 +0300

    hm2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.google.inject:guice:5.1.0")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.slf4j:slf4j-api:2.0.3")
+
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,5 +1,12 @@
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
-        
+        Injector injector = Guice.createInjector(new LoggerModule());
+        injector.getInstance(MsgLog.class);
     }
 }
